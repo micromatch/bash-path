16ae86ee86cade97e41809dbf4906ea7c04b7a00
jonschlinkert <jon.schlinkert@sellside.com>
2017-10-19 11:10:45 -0400

    add more default paths

--- PATH.js
+++ PATH.js
@@ -1,12 +1,22 @@
 'use strict';
 
+var union = require('arr-union');
 var path = require('path');
+var defaults = [
+  '/bin/bash',
+  '/sbin/bash',
+  '/usr/bin/bash',
+  '/usr/local/bin/bash',
+  '/usr/local/sbin/bash',
+  '/usr/sbin/bash',
+  '/usr/X11/bin/bash',
+  '/opt/local/bin',
+  '/opt/local/sbin',
+  'bash'
+];
 
 module.exports = function(compare) {
-  var paths = process.env.PATH.split(path.delimiter);
-  if (paths.indexOf('/usr/local/bin/bash') === -1) paths.push('/usr/local/bin/bash');
-  if (paths.indexOf('/bin/bash') === -1) paths.push('/bin/bash');
-  if (paths.indexOf('bash') === -1) paths.push('bash');
+  var paths = union([], defaults, process.env.PATH.split(path.delimiter));
   if (typeof compare === 'function') {
     paths.sort(compare);
     return paths;
